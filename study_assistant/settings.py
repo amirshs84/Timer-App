ef2548a3eac5b09a842dba4c250cfe8befd5ae31
AmirH <ahosain.hashemi.com@gmail.com>
2026-02-20 16:03:00 +0330

    domain added

--- study_assistant/settings.py
+++ study_assistant/settings.py
@@ -65,6 +65,9 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173').split(',')
 CORS_ALLOW_CREDENTIALS = True
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173').split(',')
+
+
 ROOT_URLCONF = 'study_assistant.urls'
 
 TEMPLATES = [
