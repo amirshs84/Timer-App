ed20e294727d1a1a7681517933d73f505000a529
AmirH <ahosain.hashemi.com@gmail.com>
2026-02-20 17:46:03 +0330

    arvan fixed

--- study_assistant/settings.py
+++ study_assistant/settings.py
@@ -33,6 +33,12 @@ DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
+# Security settings for running behind a proxy (ArvanCloud/Cloudflare)
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+USE_X_FORWARDED_HOST = True
+USE_X_FORWARDED_PORT = True
+
+
 
 # Application definition
 
